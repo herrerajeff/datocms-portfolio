761dda83892360a426056e0efa8ddc5407143303
Jeffrey Herrera <herrera.jeffrey@gmail.com>
2021-06-24 11:50:53 -0400

    Remove Casa array in favor for Prismic Graphql

--- src/pages/casa.js
+++ src/pages/casa.js
@@ -5,44 +5,6 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import CasaCard from "../components/casa_card"
 
-// const DesignLinks = [
-//   { name: "Brand New", url: "http://www.underconsideration.com/brandnew/" },
-//   { name: "Dieline", url: "http://www.thedieline.com/" },
-//   { name: "SiteInspire", url: "http://siteinspire.com/" },
-//   { name: "Httpster", url: "https://httpster.net/" },
-//   { name: "Front End Front", url: "https://frontendfront.com/" },
-//   { name: "Khroma", url: "http://khroma.co/generator/" },
-//   { name: "Trace", url: "https://www.stickermule.com/trace" },
-// ]
-
-// const MiscLinks = [
-//   { name: "YouTube", url: "https://www.youtube.com/feed/subscriptions" },
-//   { name: "Spotify", url: "https://open.spotify.com/" },
-//   { name: "Twitch", url: "https://www.twitch.tv/directory/following" },
-//   { name: "Twitter", url: "https://www.twitter.com" },
-//   { name: "Amazon", url: "https://smile.amazon.com" },
-//   { name: "Seamless", url: "https://www.seamless.com" },
-//   { name: "Netflix", url: "https://www.netflix.com" },
-//   {
-//     name: "Prime Video",
-//     url:
-//       "https://www.amazon.com/Amazon-Video/b/?&node=2858778011&ref=dvm_MLP_ROWNA_US_1",
-//   },
-//   { name: "Hulu", url: "https://www.hulu.com" },
-//   { name: "HBO", url: "https://www.hbomax.com" },
-// ]
-
-// const DevLinks = [
-//   { name: "SVG OMG", url: "https://jakearchibald.github.io/svgomg/" },
-//   { name: "PNG IMG", url: "http://pngimg.com/" },
-//   { name: "Brand Colors", url: "https://brandcolors.net/" },
-//   { name: "TinyPNG", url: "https://tinypng.com/" },
-//   { name: "Gif Optimizer", url: "https://ezgif.com/optimize" },
-//   { name: "Mesh Gradient", url: "https://www.meshgradient.com" },
-//   { name: "Spline", url: "https://www.spline.design" },
-//   { name: "NounProject", url: "https://thenounproject.com" },
-// ]
-
 const Home = ({ data }) => {
   const document = data.allPrismicCasa.edges[0].node.data.link
 

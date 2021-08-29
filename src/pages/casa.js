6c65c83d9349ee8f574a3f4237b001528e6ddfae
Jeffrey Herrera <herrera.jeffrey@gmail.com>
2021-08-28 22:37:30 -0400

    Casa Header Update

--- src/pages/casa.js
+++ src/pages/casa.js
@@ -13,13 +13,10 @@ const Home = ({ data }) => {
   return (
     <Layout>
       <Seo title="Casa" />
-      <div className="container md:mt-24">
+      <div className="container xl:mt-24">
         <h1 className="mx-auto text-center mb-8 text-yellow-400">Casa.</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 items-start overflow-hidden">
           <div className="grid grid-rows-auto bg-gray-900/30 hover:bg-yellow-400/10 transition duration-300 rounded-xl py-4 border border-white/10 first:rounded-xl group">
-            <h2 className="mb-4 p-4 border-b border-white/10 group-hover:text-yellow-400 transition duration-300">
-              Design
-            </h2>
             <div className="grid grid-cols-2 grid-rows-auto gap-4 p-4">
               {design.map(link => {
                 return <CasaCard url={link.url} name={link.title} />

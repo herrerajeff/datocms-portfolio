594d8c9c3a74b664647c4bd22526577d80cff7ed
Jeffrey Herrera <herrera.jeffrey@gmail.com>
2021-09-05 18:39:18 -0400

    Add 404 Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,7 +10,7 @@ const NotFoundPage = () => (
     <div className="mx-8 md:absolute md:inset-0 text-center flex flex-col justify-center items-center">
       <div className=" md:bg-black/60 p-12 md:p-32 rounded-xl md:border border-white/10 md:shadow-2xl">
         <h1 className="block text-9xl font-light text-yellow-400">404</h1>
-        <p className="max-w-lg text-lg md:text-xl leading-relaxed">
+        <p className="max-w-xl text-lg md:text-xl leading-relaxed">
           Looks like the cat just knocked your link off the table...use the
           navigation to get back to a page that exists.
         </p>

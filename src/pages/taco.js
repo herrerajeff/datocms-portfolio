a191b22fa6b9c2f20142bee21b5a8e5a8d06a8ed
Jeffrey Herrera <herrera.jeffrey@gmail.com>
2021-09-07 12:18:34 -0400

    Taco Page Marquee Fix

--- src/pages/taco.js
+++ src/pages/taco.js
@@ -11,33 +11,29 @@ const Taco = ({ data }) => {
   return (
     <Layout background={document.background.url}>
       <Seo title="taco" />
-      <div>
-        <div className="text-center">
-          <h2
-            className="inline-block text-6xl font-serif leading-tight taco-marquee"
-            style={{
-              fontFamily: "Comic Sans MS, Comic Sans, cursive",
-              color: "lime",
-            }}
-          >
-            {document.title}
-          </h2>
-        </div>
-        <div className="text-center mt-8 container">
-          <GatsbyImage
-            className="border-2 border-white taco-spin w-1/2 mx-auto"
-            image={document.featuredImage.gatsbyImageData}
-            alt="Jeff Herrera Portrait"
-          />
-          <div className="flex md:justify-between items-center flex-wrap justify-center mx-auto mt-12">
-            {document.glitters.map(image => {
-              return (
-                <div className="object-contain w-72">
-                  <GatsbyImage image={image.gatsbyImageData} />
-                </div>
-              )
-            })}
-          </div>
+      <h2
+        className="absolute block text-3xl md:text-6xl font-serif leading-tight z-10 left-0 right-0 -mx-8 taco-marquee"
+        style={{
+          fontFamily: "Comic Sans MS, Comic Sans, cursive",
+          color: "lime",
+        }}
+      >
+        {document.title}
+      </h2>
+      <div className="text-center mt-12 container">
+        <GatsbyImage
+          className="border-2 border-white taco-spin w-full md:w-1/2 mx-auto"
+          image={document.featuredImage.gatsbyImageData}
+          alt="Jeff Herrera Portrait"
+        />
+        <div className="flex md:justify-between items-center flex-wrap justify-center mx-auto mt-12">
+          {document.glitters.map(image => {
+            return (
+              <div className="object-contain w-72 mb-8">
+                <GatsbyImage image={image.gatsbyImageData} />
+              </div>
+            )
+          })}
         </div>
       </div>
     </Layout>

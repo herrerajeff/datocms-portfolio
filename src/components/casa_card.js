88fdf84a9524f9ce3303ef5081a181e89ae6d694
Jeffrey Herrera <herrera.jeffrey@gmail.com>
2022-02-23 21:10:27 -0500

    Casa styling

--- src/components/casa_card.js
+++ src/components/casa_card.js
@@ -7,7 +7,7 @@ const CasaCard = ({ url, name }) => {
       target="_blank"
       rel="noreferrer nofollower"
       to="/"
-      className="text-sm tracking-wider px-3 py-2 rounded-full bg-white/5 hover:bg-yellow-400 hover:text-black transition duration-300 flex items-center"
+      className="text-xs font-medium tracking-wider px-3 py-2 rounded-full bg-white/5 hover:text-white hover:bg-white/40 transition duration-300 flex items-center"
     >
       <img
         src={`https://www.google.com/s2/favicons?domain_url=${url}`}
